e09650ca7aa01fa998511978f8a9451d45fe5781
jinhoLee <kswlsgh@naver.com>
2024-01-22 19:59:03 +0900

    first commit

---
+++ src/main/java/dnd/donworry/DonworryApplication.java
@@ -0,0 +1,13 @@
+package dnd.donworry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonworryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonworryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dnd/donworry/DonworryApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd.donworry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonworryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
