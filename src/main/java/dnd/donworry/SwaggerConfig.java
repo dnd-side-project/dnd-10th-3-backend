7f45b8b73420442ceafcb93ad99e7c5ab91a24fc
jinhoLee <kswlsgh@naver.com>
2024-01-25 00:18:37 +0900

    ✅ webhooks Test

--- src/main/java/dnd/donworry/SwaggerConfig.java
+++ src/main/java/dnd/donworry/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("API Test") // API의 제목
+                .title("DonWorry") // API의 제목
                 .description("DonWorry Swagger UI") // API에 대한 설명
                 .version("1.0.0"); // API의 버전
     }
