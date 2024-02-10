df93088d144292bf01213ccda0426e58fade7467
dlrjs2360 <lhg3670@naver.com>
2024-02-11 01:26:31 +0900

    ✨ 익셉션 처리 클래스 생성
    예측가능한 에러를 익셉션으로 처리하기 위한 CustomException 클래스를 생성하고 GlobalExceptionHandler에서 이를 탐지하여 클라이언트에 전달합니다.
    
    회원관리 기능과 통합하여 JwtFilter가 추가되면 필터에서 발생하는 익셉션은 ExceptionHandleFilter에서 처리합니다.
    
    ErrorCode와 ResponseCode에서 사용하지 않는 Status를 삭제했습니다.

--- src/main/java/dnd/donworry/domain/constants/ResponseCode.java
+++ src/main/java/dnd/donworry/domain/constants/ResponseCode.java
@@ -1,7 +1,5 @@
 package dnd.donworry.domain.constants;
 
-import org.springframework.http.HttpStatus;
-
 import jakarta.annotation.Nullable;
 import lombok.Getter;
 import lombok.ToString;
@@ -10,44 +8,41 @@ import lombok.ToString;
 @ToString
 public enum ResponseCode {
 	/*TEST*/
-	TEST_SAVED(HttpStatus.OK, "200", "테스트 결과 저장에 성공했습니다."),
-	TEST_SUCCESS(HttpStatus.OK, "200", "테스트 결과 생성에 성공했습니다."),
-	TEST_FIND_SUCCESS(HttpStatus.OK, "200", "테스트 결과 조회에 성공했습니다."),
+	TEST_SAVED("200", "테스트 결과 저장에 성공했습니다."),
+	TEST_SUCCESS("200", "테스트 결과 생성에 성공했습니다."),
+	TEST_FIND_SUCCESS("200", "테스트 결과 조회에 성공했습니다."),
 
 	/*AUTH*/
-	MEMBER_SAVE(HttpStatus.OK, "200", "회원가입에 성공했습니다."),
-	MEMBER_LOGIN(HttpStatus.OK, "200", "로그인에 성공했습니다"),
-	MEMBER_LOGOUT(HttpStatus.OK, "200", "로그아웃에 성공했습니다."),
-	VERIFICATION_SUCCESS(HttpStatus.OK, "200", "인증에 성공했습니다."),
+	MEMBER_SAVE("200", "회원가입에 성공했습니다."),
+	MEMBER_LOGIN("200", "로그인에 성공했습니다"),
+	MEMBER_LOGOUT("200", "로그아웃에 성공했습니다."),
+	VERIFICATION_SUCCESS("200", "인증에 성공했습니다."),
 
 	/* MEMBER */
-	MEMBER_DETAIL(HttpStatus.OK, "200", "회원정보 불러오기에 성공했습니다."),
-	MEMBER_UPDATE(HttpStatus.OK, "200", "회원정보 수정에 성공했습니다."),
-	MEMBER_DELETE(HttpStatus.OK, "200", "회원정보 삭제에 성공했습니다."),
-	MEMBER_EXISTS(HttpStatus.OK, "200", "회원존재 여부 조회에 성공했습니다."),
-	AVATAR_UPLOAD(HttpStatus.OK, "200", "이미지 업로드에 성공했습니다."),
-	AVATAR_DELETE(HttpStatus.OK, "200", "이미지 삭제에 성공했습니다."),
+	MEMBER_DETAIL("200", "회원정보 불러오기에 성공했습니다."),
+	MEMBER_UPDATE("200", "회원정보 수정에 성공했습니다."),
+	MEMBER_DELETE("200", "회원정보 삭제에 성공했습니다."),
+	MEMBER_EXISTS("200", "회원존재 여부 조회에 성공했습니다."),
+	AVATAR_UPLOAD("200", "이미지 업로드에 성공했습니다."),
+	AVATAR_DELETE("200", "이미지 삭제에 성공했습니다."),
 
 	/* VOTE */
-	VOTING_SUCCESS(HttpStatus.OK, "200", "투표 생성에 성공했습니다."),
-	VOTING_UPDATE(HttpStatus.OK, "200", "투표 수정에 성공했습니다."),
-	VOTING_DELETE(HttpStatus.OK, "200", "투표 취소에 성공했습니다."),
+	VOTING_SUCCESS("200", "투표 생성에 성공했습니다."),
+	VOTING_UPDATE("200", "투표 수정에 성공했습니다."),
+	VOTING_DELETE("200", "투표 취소에 성공했습니다."),
 
 	/* LIKES */
-	LIKES_ADD(HttpStatus.OK, "200", "좋아요 추가에 성공했습니다."),
-	LIKES_CANCEL(HttpStatus.OK, "200", "좋아요 취소에 성공했습니다."),
+	LIKES_ADD("200", "좋아요 추가에 성공했습니다."),
+	LIKES_CANCEL("200", "좋아요 취소에 성공했습니다."),
 
 	/* SEARCH */
-	SEARCH_SUCCESS(HttpStatus.OK, "200", "검색에 성공했습니다.");
-
-	private final HttpStatus httpStatus;
+	SEARCH_SUCCESS("200", "검색에 성공했습니다.");
 
 	private final String code;
 
 	private final String message;
 
-	ResponseCode(HttpStatus httpStatus, String code, String message) {
-		this.httpStatus = httpStatus;
+	ResponseCode(String code, String message) {
 		this.code = code;
 		this.message = message;
 	}
---
+++ src/main/java/dnd/donworry/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package dnd.donworry.exception;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import dnd.donworry.domain.constants.ErrorCode;
+import dnd.donworry.domain.constants.ResResult;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(CustomException.class)
+	protected ResResult<?> handlerCustomException(CustomException e) {
+		log.error("CustomException: " + e.getErrorCode().getMessage());
+		return e.getErrorCode().toResponse(null);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResResult<?> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException e, HttpHeaders h, HttpStatus s, WebRequest w
+	) {
+		Map<String, String> validExceptions = e.getBindingResult().getFieldErrors().stream()
+			.collect(Collectors.toMap(
+				FieldError::getField,
+				DefaultMessageSourceResolvable::getDefaultMessage
+			));
+		log.error("Validation Exception: " + validExceptions);
+		return ErrorCode.INVALID_REQUEST.toResponse(validExceptions);
+	}
+
+	@ExceptionHandler(Exception.class)
+	protected ResResult<?> handlerException(Exception e) {
+		log.error("Unexpected_Exception : " + e.getMessage());
+		log.error("Unexpected_Exception : " + Arrays.toString(e.getStackTrace()));
+		return ErrorCode.UNEXPECTED_EXCEPTION.toResponse(null);
+	}
+}
