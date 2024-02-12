5dcee4559e6fa3b989f6b37dc0ca361e24be9fdb
dlrjs2360 <lhg3670@naver.com>
2024-02-12 17:40:21 +0900

    📝 투표 생성 API 문서 작성
    투표를 생성할 때 발생할 수 있는 에러와 성공 Response를 작성했습니다.
    
    에러 Response 중 404에러는 후에 유효성 검사가 도입되면 어떤 필드가 잘못되었는지 표시합니다.

--- src/main/java/dnd/donworry/controller/VoteController.java
+++ src/main/java/dnd/donworry/controller/VoteController.java
@@ -14,6 +14,11 @@ import dnd.donworry.domain.constants.ResponseCode;
 import dnd.donworry.domain.dto.vote.VoteRequestDto;
 import dnd.donworry.domain.dto.vote.VoteResponseDto;
 import dnd.donworry.service.VoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +31,25 @@ public class VoteController {
 	private final VoteService voteService;
 
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Operation(summary = "투표 생성", description = "인증된 회원이 투표를 생성합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "투표 생성 성공"),
+		@ApiResponse(responseCode = "400", description = "투표 생성 실패", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"400\", \n \"message\": \"투표 생성에 실패했습니다.\"\n}"))),
+		@ApiResponse(responseCode = "404", description = "투표 생성 실패", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"404\", \n \"message\": \"입력값이 잘못되었습니다.\"\n}"))),
+		@ApiResponse(responseCode = "403", description = "접근 권한 없음", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"403\", \n \"message\": \"접근 권한이 없습니다.\"\n}"))),
+		@ApiResponse(responseCode = "401", description = "토큰이 존재하지 않음", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"401\", \n \"message\": \"유효한 토큰이 존재하지 않습니다.\"\n}"))),
+		@ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"500\", \n \"message\": \"서버에 에러가 발생했습니다.\"\n}")))
+	})
 	public ResResult<VoteResponseDto> create(@RequestPart VoteRequestDto voteRequestDto,
 		@RequestPart List<MultipartFile> images) {
 		String username = "test"; // Authentication.getName()으로 변경
