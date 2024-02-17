64eb8e5b10d9c5214e1ec18f1583cb8b58ccb670
dlrjs2360 <lhg3670@naver.com>
2024-02-17 22:20:04 +0900

    :sparkles: 스웨거 유저 생성 기능 구현
    스웨거에서 사용할 수 있는 유저를 생성하고 바로 로그인할 수 있는 기능을 구현했습니다.

--- src/main/java/dnd/donworry/controller/OauthController.java
+++ src/main/java/dnd/donworry/controller/OauthController.java
@@ -1,8 +1,14 @@
 package dnd.donworry.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import dnd.donworry.domain.constants.ResResult;
 import dnd.donworry.domain.constants.ResponseCode;
 import dnd.donworry.domain.dto.jwt.TokenResponseDto;
+import dnd.donworry.domain.dto.user.UserResponseDto;
 import dnd.donworry.service.OauthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,9 +20,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
@@ -24,17 +27,30 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Oauth API", description = "소셜 로그인을 통해 유저 로그인, 회원가입 합니다.")
 public class OauthController {
 
-    private final OauthService oauthService;
+	private final OauthService oauthService;
 
-    @GetMapping("/login/oauth2/code/kakao")
-    @Operation(summary = "로그인 및 회원가입", description = "카카오 로그인 API로 로그인 및 회원가입 합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "로그인 및 회원가입 성공"),
-            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(
-                    mediaType = "application/json",
-                    examples = @ExampleObject(value = "{\n  \"code\": \"500\", \n \"message\": \"서버에 에러가 발생했습니다.\"\n}")))
-    })
-    public ResResult<TokenResponseDto> login(@Valid @RequestParam(value = "code") String code, HttpServletResponse response) {
-        return ResponseCode.MEMBER_LOGIN.toResponse(oauthService.loginWithKakao(code, response));
-    }
+	@GetMapping("/login/oauth2/code/kakao")
+	@Operation(summary = "로그인 및 회원가입", description = "카카오 로그인 API로 로그인 및 회원가입 합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "로그인 및 회원가입 성공"),
+		@ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"500\", \n \"message\": \"서버에 에러가 발생했습니다.\"\n}")))
+	})
+	public ResResult<TokenResponseDto> login(@Valid @RequestParam(value = "code") String code,
+		HttpServletResponse response) {
+		return ResponseCode.MEMBER_LOGIN.toResponse(oauthService.loginWithKakao(code, response));
+	}
+
+	@PostMapping("/swaggerUser")
+	@Operation(summary = "스웨거 유저 생성", description = "스웨거를 위한 유저를 생성하고 로그인합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "스웨거 유저 생성 성공"),
+		@ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(
+			mediaType = "application/json",
+			examples = @ExampleObject(value = "{\n  \"code\": \"500\", \n \"message\": \"서버에 에러가 발생했습니다.\"\n}")))
+	})
+	public ResResult<UserResponseDto.UPDATE> createSwaggerUser(HttpServletResponse response) {
+		return ResponseCode.MEMBER_SAVE.toResponse(oauthService.createSwaggerUser(response));
+	}
 }
--- src/main/java/dnd/donworry/service/impl/UserServiceImpl.java
+++ src/main/java/dnd/donworry/service/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package dnd.donworry.service.impl;
 
-import dnd.donworry.domain.entity.User;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import dnd.donworry.domain.constants.ErrorCode;
 import dnd.donworry.domain.dto.user.UserResponseDto;
+import dnd.donworry.domain.entity.User;
 import dnd.donworry.exception.CustomException;
 import dnd.donworry.repository.RefreshTokenRepository;
 import dnd.donworry.repository.UserRepository;
@@ -11,45 +16,41 @@ import dnd.donworry.util.CookieUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final CookieUtil cookieUtil;
+	private final UserRepository userRepository;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final CookieUtil cookieUtil;
 
-    @Transactional
-    public void deleteUser(String email) {
-        Optional<User> user = userRepository.findUserByEmail(email);
-        user.ifPresent(userRepository::delete);
-        if (refreshTokenRepository.findById(email).isPresent()) {
-            refreshTokenRepository.deleteById(email);
-        }
-    }
+	@Transactional
+	public void deleteUser(String email) {
+		Optional<User> user = userRepository.findUserByEmail(email);
+		user.ifPresent(userRepository::delete);
+		if (refreshTokenRepository.findById(email).isPresent()) {
+			refreshTokenRepository.deleteById(email);
+		}
+	}
 
-    @Transactional
-    public UserResponseDto.UPDATE updateUserNickname(String email, String nickname) {
-        User user = userRepository.findUserByEmail(email).get();
+	@Transactional
+	public UserResponseDto.UPDATE updateUserNickname(String email, String nickname) {
+		User user = userRepository.findUserByEmail(email).get();
 
-        if (user.getNickname().equals(nickname) || userRepository.existsByNickname(nickname)) {
-            throw new CustomException(ErrorCode.NICKNAME_DUPLICATION);
-        }
+		if (user.getNickname().equals(nickname) || userRepository.existsByNickname(nickname)) {
+			throw new CustomException(ErrorCode.NICKNAME_DUPLICATION);
+		}
 
-        user.updateNickname(nickname);
-        userRepository.save(user);
+		user.updateNickname(nickname);
+		userRepository.save(user);
 
-        return UserResponseDto.UPDATE.of(user);
-    }
+		return UserResponseDto.UPDATE.of(user);
+	}
 
-    @Transactional
-    public void logout(String email, HttpServletResponse response) {
-        cookieUtil.deleteCookie(response, "ACCESS_TOKEN", "REFRESH_TOKEN");
-        refreshTokenRepository.deleteById(email);
-    }
+	@Transactional
+	public void logout(String email, HttpServletResponse response) {
+		cookieUtil.deleteCookie(response, "ACCESS_TOKEN", "REFRESH_TOKEN");
+		refreshTokenRepository.deleteById(email);
+	}
 }
