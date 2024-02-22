f14c89bea9dafc6ca16cb7fb321b11d7a9e7a540
wlshooo <kswlsgh@naver.com>
2024-02-22 21:33:12 +0900

    📝comment 페이징 api swagger 추가

--- src/main/java/dnd/donworry/controller/CommentController.java
+++ src/main/java/dnd/donworry/controller/CommentController.java
@@ -121,6 +121,22 @@ public class CommentController {
     }
 
     @GetMapping("/{voteId}")
+    @Operation(summary = "댓글 페이징 조회 ", description = "인증된 회원이 댓글을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "조회 실패", content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n  \"code\": \"404\", \n \"message\": \"입력값이 잘못되었습니다.\"\n}"))),
+            @ApiResponse(responseCode = "403", description = "접근 권한 없음", content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n  \"code\": \"403\", \n \"message\": \"접근 권한이 없습니다.\"\n}"))),
+            @ApiResponse(responseCode = "401", description = "토큰이 존재하지 않음", content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n  \"code\": \"401\", \n \"message\": \"유효한 토큰이 존재하지 않습니다.\"\n}"))),
+            @ApiResponse(responseCode = "500", description = "서버 에러", content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\n  \"code\": \"500\", \n \"message\": \"서버에 에러가 발생했습니다.\"\n}")))
+    })
     ResResult<List<CommentResponseDto>> getComments(@PathVariable(name = "voteId") Long voteId,
                                                     @RequestParam(value = "lastCommentId", defaultValue = "0") Long lastCommentId,
                                                     @RequestParam(value = "size", defaultValue = "2") int size,
