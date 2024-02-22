19d550f6cdc7e035c2a2f0c28492d46eed3f0200
dlrjs2360 <lhg3670@naver.com>
2024-02-22 20:56:37 +0900

    :bug: 로그아웃 수정
    로그아웃 시 토큰 키 값이 달랐던 버그를 수정합니다.

--- src/main/java/dnd/donworry/service/impl/OauthServiceImpl.java
+++ src/main/java/dnd/donworry/service/impl/OauthServiceImpl.java
@@ -1,5 +1,10 @@
 package dnd.donworry.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import dnd.donworry.domain.dto.jwt.TokenDto;
 import dnd.donworry.domain.dto.user.LoginResponseDto;
 import dnd.donworry.domain.dto.user.UserDto;
@@ -15,60 +20,57 @@ import dnd.donworry.util.CookieUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class OauthServiceImpl implements OauthService {
-    private final JwtProvider jwtProvider;
-    private final KakaoOauthService kakaoOauthService;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final CookieUtil cookieUtil;
-    private final UserRepository userRepository;
+	private static final String ACCESS_TOKEN = "Access-Token";
+	private static final String REFRESH_TOKEN = "Refresh-Token";
+	private final JwtProvider jwtProvider;
+	private final KakaoOauthService kakaoOauthService;
+	private final RefreshTokenRepository refreshTokenRepository;
+	private final CookieUtil cookieUtil;
+	private final UserRepository userRepository;
 
-    private final String ACCESS_TOKEN = "Access-Token";
-    private final String REFRESH_TOKEN = "Refresh-Token";
-
-    @Transactional
-    public LoginResponseDto loginWithKakao(String code, HttpServletResponse response) {
-        UserDto userDto = kakaoOauthService.login(code);
-        TokenDto tokenDto = jwtProvider.generateJwtToken(userDto.getEmail());
-        createToken(userDto.getEmail(), response);
+	@Transactional
+	public LoginResponseDto loginWithKakao(String code, HttpServletResponse response) {
+		UserDto userDto = kakaoOauthService.login(code);
+		TokenDto tokenDto = jwtProvider.generateJwtToken(userDto.getEmail());
+		createToken(userDto.getEmail(), response);
 		log.info("oauthServiceImpl.code = {}", code);
-		log.info("loginWithKakao.nickname = {}",userDto.getNickname());
-        return LoginResponseDto.builder()
-                .accessToken(tokenDto.getAccessToken())
-                .refreshToken(tokenDto.getRefreshToken())
-                .nickname(userDto.getNickname())
-                .build();
-    }
+		log.info("loginWithKakao.nickname = {}", userDto.getNickname());
+		return LoginResponseDto.builder()
+			.accessToken(tokenDto.getAccessToken())
+			.refreshToken(tokenDto.getRefreshToken())
+			.nickname(userDto.getNickname())
+			.build();
+	}
 
-    @Transactional
-    public void createToken(String email, HttpServletResponse response) {
-        TokenDto tokenDto = jwtProvider.generateJwtToken(email);
-        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByValue(email);
-        refreshToken.ifPresent(refreshTokenRepository::delete);
-        refreshTokenRepository.save(
-                new RefreshToken(tokenDto.getRefreshToken(), email, jwtProvider.getRefreshTokenExpiredTime()));
-        cookieUtil.setCookie(response, REFRESH_TOKEN, tokenDto.getRefreshToken(), jwtProvider.getRefreshTokenExpiredTime());
-        cookieUtil.setCookie(response, ACCESS_TOKEN, tokenDto.getAccessToken(), jwtProvider.getAccessTokenExpiredTime());
-    }
+	@Transactional
+	public void createToken(String email, HttpServletResponse response) {
+		TokenDto tokenDto = jwtProvider.generateJwtToken(email);
+		Optional<RefreshToken> refreshToken = refreshTokenRepository.findByValue(email);
+		refreshToken.ifPresent(refreshTokenRepository::delete);
+		refreshTokenRepository.save(
+			new RefreshToken(tokenDto.getRefreshToken(), email, jwtProvider.getRefreshTokenExpiredTime()));
+		cookieUtil.setCookie(response, REFRESH_TOKEN, tokenDto.getRefreshToken(),
+			jwtProvider.getRefreshTokenExpiredTime());
+		cookieUtil.setCookie(response, ACCESS_TOKEN, tokenDto.getAccessToken(),
+			jwtProvider.getAccessTokenExpiredTime());
+	}
 
-    @Transactional
-    @Override
-    public UserResponseDto.UPDATE createSwaggerUser(HttpServletResponse response) {
-        User user = User.builder()
-                .email("test@test.com")
-                .nickname("test")
-                .avatar("")
-                .build();
-        userRepository.save(user);
-        log.info("Swagger User Created");
-        createToken(user.getEmail(), response);
-        return UserResponseDto.UPDATE.of(user);
-    }
+	@Transactional
+	@Override
+	public UserResponseDto.UPDATE createSwaggerUser(HttpServletResponse response) {
+		User user = User.builder()
+			.email("test@test.com")
+			.nickname("test")
+			.avatar("")
+			.build();
+		userRepository.save(user);
+		log.info("Swagger User Created");
+		createToken(user.getEmail(), response);
+		return UserResponseDto.UPDATE.of(user);
+	}
 }
